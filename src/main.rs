32796c20bd3c042e2bb8b4aa91027053cbd016fd
malta895 <lmalta95@gmail.com>
2022-11-07 23:34:28 +0100

    show a simple web page

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
+const HELLO_HTML_PATH: &str = "./www/hello.html";
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -23,4 +25,15 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = match fs::read_to_string(HELLO_HTML_PATH) {
+        Ok(file_content) => file_content,
+        Err(error) => format!("Cannot display file {HELLO_HTML_PATH}: {error}"),
+    };
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Lenght: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
